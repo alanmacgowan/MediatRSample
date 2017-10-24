9c51956af2cf0c84cf59452fc8931a1323223fa1
Alan Macgowan <alanmacgowan@gmail.com>
2017-10-23 23:27:55 -0300

    added database initialization

---
+++ MediatRSample/Infrastructure/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using MediatRSample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediatRSample.Infrastructure
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(OrderProcessingContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Customers.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var customers = new Customer[]
+            {
+            new Customer{FirstName="Juan",LastName="Perez",Email="juanperez@lalal.com", Address = new Address {City="CABA", County = "Buenos Aires", PostCode = "BSHD66D", Street="Calle 12" } },
+            new Customer{FirstName="Jose",LastName="Pareas",Email="jose1@lalal.com", Address = new Address {City="Rosario", County = "Santa Fe", PostCode = "DDSS33", Street="Calle 22" }},
+            new Customer{FirstName="Pablo",LastName="Lopez",Email="xxxxx@xxxx.com", Address = new Address {City="CABA", County = "Buenos Aires", PostCode = "344DFDFG", Street="Calle 24" }},
+            new Customer{FirstName="Carlos",LastName="Gonzalez",Email="Gonzalez@hotmail.com", Address = new Address {City="CABA", County = "Buenos Aires", PostCode = "34444FDF", Street="Calle 3" }},
+            };
+
+            foreach (Customer c in customers)
+            {
+                context.Customers.Add(c);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
--- MediatRSample/Program.cs
+++ MediatRSample/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using MediatRSample.Infrastructure;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.IO;
 
 namespace MediatRSample
@@ -9,6 +13,22 @@ namespace MediatRSample
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<OrderProcessingContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
             host.Run();
         }
 
